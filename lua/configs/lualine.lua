bc3c24df7bccab5a192754808e827ffefb297a8f
devsheke <abhi@sheke.tech>
2024-11-17 14:01:51 +0530

    feat: update colors to rose-pine dawn

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -1,17 +1,20 @@
 local colors = {
-	fg = "#e0def4",
-	bg = "#26233a",
-	darkblue = "#9ccfd8",
-	orange = "#eb6f92",
-	violet = "#c4a7e7",
-	black = "#26233a",
-	red = "#eb6f92",
-	green = "#31748f",
-	yellow = "#f6c177",
-	blue = "#9ccfd8",
-	magenta = "#c4a7e7",
-	cyan = "#ebbcba",
-	white = "#e0def4",
+	base = "#faf4ed",
+	surface = "#fffaf3",
+	overlay = "#f2e9e1",
+	muted = "#9893a5",
+	subtle = "#797593",
+	text = "#575279",
+	love = "#b4637a",
+	gold = "#ea9d34",
+	rose = "#d7827e",
+	pine = "#286983",
+	foam = "#56949f",
+	iris = "#907aa9",
+	leaf = "#6d8f89",
+	highlight_low = "#f4ede8",
+	highlight_med = "#dfdad9",
+	highlight_high = "#cecacd",
 }
 
 local conditions = {
@@ -33,8 +36,8 @@ local options = {
 		component_separators = "",
 		section_separators = "",
 		theme = {
-			normal = { c = { fg = colors.fg, bg = colors.bg } },
-			inactive = { c = { fg = colors.fg, bg = colors.bg } },
+			normal = { c = { fg = colors.text, bg = colors.overlay } },
+			inactive = { c = { fg = colors.text, bg = colors.overlay } },
 		},
 	},
 	sections = {
@@ -67,7 +70,7 @@ ins_left({
 	function()
 		return "▊"
 	end,
-	color = { fg = colors.blue },
+	color = { fg = colors.foam },
 	padding = { left = 0, right = 0 },
 })
 
@@ -75,28 +78,28 @@ ins_left({
 	"mode",
 	color = function()
 		local mode_color = {
-			n = colors.red,
-			i = colors.green,
-			v = colors.blue,
-			[""] = colors.blue,
-			V = colors.blue,
-			c = colors.magenta,
-			no = colors.red,
-			s = colors.orange,
-			S = colors.orange,
-			[""] = colors.orange,
-			ic = colors.yellow,
-			R = colors.violet,
-			Rv = colors.violet,
-			cv = colors.red,
-			ce = colors.red,
-			r = colors.cyan,
-			rm = colors.cyan,
-			["r?"] = colors.cyan,
-			["!"] = colors.red,
-			t = colors.red,
+			n = colors.love,
+			i = colors.pine,
+			v = colors.foam,
+			[""] = colors.foam,
+			V = colors.foam,
+			c = colors.iris,
+			no = colors.love,
+			s = colors.gold,
+			S = colors.gold,
+			[""] = colors.gold,
+			ic = colors.gold,
+			R = colors.love,
+			Rv = colors.love,
+			cv = colors.love,
+			ce = colors.love,
+			r = colors.rose,
+			rm = colors.rose,
+			["r?"] = colors.rose,
+			["!"] = colors.love,
+			t = colors.love,
 		}
-		return { bg = mode_color[vim.fn.mode()], fg = "#000000", gui = "bold" }
+		return { bg = mode_color[vim.fn.mode()], fg = colors.highlight_low, gui = "bold" }
 	end,
 	padding = { right = 1, left = 1 },
 })
@@ -104,22 +107,22 @@ ins_left({
 ins_left({
 	"filename",
 	cond = conditions.buffer_not_empty,
-	color = { fg = colors.magenta, gui = "bold" },
+	color = { fg = colors.iris, gui = "bold" },
 })
 
 ins_left({
 	"branch",
 	icon = "",
-	color = { fg = colors.violet, gui = "bold" },
+	color = { fg = colors.love, gui = "bold" },
 })
 
 ins_left({
 	"diff",
 	symbols = { added = " ", modified = "󰝤 ", removed = " " },
 	diff_color = {
-		added = { fg = colors.green },
-		modified = { fg = colors.orange },
-		removed = { fg = colors.red },
+		added = { fg = colors.pine },
+		modified = { fg = colors.gold },
+		removed = { fg = colors.love },
 	},
 	cond = conditions.hide_in_width,
 })
@@ -129,8 +132,8 @@ ins_left({
 	sources = { "nvim_diagnostic" },
 	symbols = { error = " ", warn = " ", info = " " },
 	diagnostics_color = {
-		color_error = { fg = colors.red },
-		color_warn = { fg = colors.yellow },
+		color_error = { fg = colors.love },
+		color_warn = { fg = colors.gold },
 		color_info = { fg = colors.cyan },
 	},
 })
@@ -152,13 +155,13 @@ ins_right({
 		return msg
 	end,
 	icon = " :",
-	color = { fg = colors.blue, gui = "bold" },
+	color = { fg = colors.foam, gui = "bold" },
 })
 
 ins_right({
 	"o:encoding",
 	cond = conditions.hide_in_width,
-	color = { fg = colors.green, gui = "bold" },
+	color = { fg = colors.pine, gui = "bold" },
 })
 
 ins_right({ "location" })
@@ -167,7 +170,7 @@ ins_right({
 	function()
 		return "▊"
 	end,
-	color = { fg = colors.blue },
+	color = { fg = colors.foam },
 	padding = { left = 1 },
 })
 
