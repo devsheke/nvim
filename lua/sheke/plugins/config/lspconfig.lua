dcc5a3c7e0200900aa90534e618ce320ead989e8
devsheke <abhi@sheke.tech>
2025-02-06 09:39:15 +0530

    update formatting styling

--- lua/sheke/plugins/config/lspconfig.lua
+++ lua/sheke/plugins/config/lspconfig.lua
@@ -2,100 +2,100 @@ local cmp = require("cmp_nvim_lsp")
 local lspconfig = require("lspconfig")
 
 local on_attach = function(_, bufnr)
-    local function opts(desc)
-        return { buffer = bufnr, desc = "Lsp " .. desc }
-    end
+	local function opts(desc)
+		return { buffer = bufnr, desc = "Lsp " .. desc }
+	end
 
-    local map = vim.keymap.set
+	local map = vim.keymap.set
 
-    map("n", "gd", vim.lsp.buf.declaration, opts("go to declaration"))
-    map("n", "gd", vim.lsp.buf.definition, opts("go to definition"))
-    map("n", "k", vim.lsp.buf.hover, opts("hover information"))
-    map("n", "gi", vim.lsp.buf.implementation, opts("go to implementation"))
-    map("n", "<leader>sh", vim.lsp.buf.signature_help, opts("show signature help"))
-    map("n", "<leader>rn", vim.lsp.buf.rename, opts("rename symbol"))
-    map("n", "<leader>d", vim.lsp.buf.type_definition, opts("go to type definition"))
-    map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("code action"))
-    map("n", "gr", vim.lsp.buf.references, opts("show references"))
-    map("n", "<leader>vd", vim.diagnostic.open_float, opts("open diagnostics float"))
-    map("n", "[d", vim.diagnostic.goto_next, opts("go to next diagnostics report"))
-    map("n", "]d", vim.diagnostic.goto_prev, opts("go to prev diagnostics report"))
+	map("n", "gd", vim.lsp.buf.declaration, opts("go to declaration"))
+	map("n", "gd", vim.lsp.buf.definition, opts("go to definition"))
+	map("n", "k", vim.lsp.buf.hover, opts("hover information"))
+	map("n", "gi", vim.lsp.buf.implementation, opts("go to implementation"))
+	map("n", "<leader>sh", vim.lsp.buf.signature_help, opts("show signature help"))
+	map("n", "<leader>rn", vim.lsp.buf.rename, opts("rename symbol"))
+	map("n", "<leader>d", vim.lsp.buf.type_definition, opts("go to type definition"))
+	map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts("code action"))
+	map("n", "gr", vim.lsp.buf.references, opts("show references"))
+	map("n", "<leader>vd", vim.diagnostic.open_float, opts("open diagnostics float"))
+	map("n", "[d", vim.diagnostic.goto_next, opts("go to next diagnostics report"))
+	map("n", "]d", vim.diagnostic.goto_prev, opts("go to prev diagnostics report"))
 end
 
 local on_init = function(client, _)
-    if client.supports_method("textdocument/semantictokens") then
-        client.server_capabilities.semantictokensprovider = nil
-    end
+	if client.supports_method("textdocument/semantictokens") then
+		client.server_capabilities.semantictokensprovider = nil
+	end
 end
 
 local servers = {
-    "bashls",
-    "clangd",
-    "cssls",
-    "dockerls",
-    "eslint",
-    "golangci_lint_ls",
-    "gopls",
-    "html",
-    "jsonls",
-    "lua_ls",
-    "nil_ls",
-    "pylsp",
-    "rust_analyzer",
-    "svelte",
-    "tailwindcss",
-    "taplo",
-    "templ",
-    "ts_ls",
-    "zls",
+	"bashls",
+	"clangd",
+	"cssls",
+	"dockerls",
+	"eslint",
+	"golangci_lint_ls",
+	"gopls",
+	"html",
+	"jsonls",
+	"lua_ls",
+	"nil_ls",
+	"pylsp",
+	"rust_analyzer",
+	"svelte",
+	"tailwindcss",
+	"taplo",
+	"templ",
+	"ts_ls",
+	"zls",
 }
 
 for _, server in ipairs(servers) do
-    lspconfig[server].setup({
-        on_attach = on_attach,
-        capabilities = cmp.default_capabilities(),
-        on_init = on_init,
-    })
+	lspconfig[server].setup({
+		on_attach = on_attach,
+		capabilities = cmp.default_capabilities(),
+		on_init = on_init,
+	})
 end
 
 lspconfig.lua_ls.setup({
-    on_attach = on_attach,
-    capabilities = cmp.default_capabilities(),
-    on_init = on_init,
-    settings = {
-        lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                maxpreload = 100000,
-                preloadfilesize = 10000,
-            },
-        },
-    },
+	on_attach = on_attach,
+	capabilities = cmp.default_capabilities(),
+	on_init = on_init,
+	settings = {
+		lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				maxpreload = 100000,
+				preloadfilesize = 10000,
+			},
+		},
+	},
 })
 
 lspconfig.tailwindcss.setup({
-    on_attach = on_attach,
-    capabilities = cmp.default_capabilities(),
-    on_init = on_init,
-    settings = {
-        tailwindcss = {
-            experimental = {
-                classregex = {
-                    { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-                    { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
-                    { "twMerge\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-                },
-            },
-        },
-    },
+	on_attach = on_attach,
+	capabilities = cmp.default_capabilities(),
+	on_init = on_init,
+	settings = {
+		tailwindcss = {
+			experimental = {
+				classregex = {
+					{ "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+					{ "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+					{ "twMerge\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+				},
+			},
+		},
+	},
 })
 
 vim.filetype.add({ extension = { templ = "templ" } })
 lspconfig.html.setup({
-    on_attach = on_attach,
-    capabilities = cmp.default_capabilities(),
-    on_init = on_init,
-    filetypes = { "html", "templ" },
+	on_attach = on_attach,
+	capabilities = cmp.default_capabilities(),
+	on_init = on_init,
+	filetypes = { "html", "templ" },
 })
--- lua/sheke/plugins/init.lua
+++ lua/sheke/plugins/init.lua
@@ -1,73 +1,73 @@
 local buf_open_events = { "BufReadPost", "BufNewFile" }
 
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = buf_open_events,
-        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-        build = ":TSUpdate",
-        opts = function()
-            return require("sheke.plugins.config.treesitter")
-        end,
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = buf_open_events,
+		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+		build = ":TSUpdate",
+		opts = function()
+			return require("sheke.plugins.config.treesitter")
+		end,
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            return require("sheke.plugins.config.lspconfig")
-        end,
-    },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			return require("sheke.plugins.config.lspconfig")
+		end,
+	},
 
-    { "nvim-tree/nvim-web-devicons", lazy = true },
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
 
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            {
-                "L3MON4D3/LuaSnip",
-                dependencies = "rafamadriz/friendly-snippets",
-                opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-                config = function(_, opts)
-                    require("luasnip").config.set_config(opts)
-                end,
-            },
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				"L3MON4D3/LuaSnip",
+				dependencies = "rafamadriz/friendly-snippets",
+				opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+				config = function(_, opts)
+					require("luasnip").config.set_config(opts)
+				end,
+			},
 
-            {
-                "windwp/nvim-autopairs",
-                opts = {
-                    fast_wrap = {},
-                    disable_filetype = { "TelescopePrompt", "vim" },
-                },
-                config = function(_, opts)
-                    require("nvim-autopairs").setup(opts)
+			{
+				"windwp/nvim-autopairs",
+				opts = {
+					fast_wrap = {},
+					disable_filetype = { "TelescopePrompt", "vim" },
+				},
+				config = function(_, opts)
+					require("nvim-autopairs").setup(opts)
 
-                    -- setup cmp for autopairs
-                    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-                    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-                end,
-            },
+					-- setup cmp for autopairs
+					local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+					require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+				end,
+			},
 
-            {
-                "saadparwaiz1/cmp_luasnip",
-                "hrsh7th/cmp-nvim-lua",
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-path",
-            },
-        },
+			{
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-path",
+			},
+		},
 
-        opts = function()
-            return require("sheke.plugins.config.cmp")
-        end,
-        config = function(_, opts)
-            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-            local cmp = require("cmp")
-            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-            cmp.setup(opts)
-        end,
-    },
+		opts = function()
+			return require("sheke.plugins.config.cmp")
+		end,
+		config = function(_, opts)
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			cmp.setup(opts)
+		end,
+	},
 }
