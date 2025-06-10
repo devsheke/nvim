375edc930ba6ffb4490d5c7041d7fb4f0ea1756e
devsheke <abhi@sheke.tech>
2025-06-10 20:35:22 +0530

    add comment, lualine plugins and add optional transparency to rose-pine

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,6 +1,7 @@
 return {
 	"rose-pine/neovim",
 	name = "rose-pine",
+	dependencies = { "xiyaowong/transparent.nvim" },
 	config = function()
 		require("rose-pine").setup({
 			highlight_groups = { SnacksIndent = { fg = "overlay" } },
