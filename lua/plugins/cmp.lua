986e3b5f4c9ca99979a1bac771151da167a36e15
devsheke <abhi@sheke.tech>
2025-06-10 17:37:52 +0530

    add completion support

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,115 @@
+local options = {
+	completion = { completeopt = "menu,menuone" },
+
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "nvim_lua" },
+		{ name = "path" },
+	},
+
+	window = {
+		completion = {
+			scrollbar = false,
+			side_padding = 1,
+			winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None,FloatBorder:CmpBorder",
+			border = "single",
+		},
+
+		documentation = {
+			border = "single",
+			winhighlight = "Normal:CmpDoc,FloatBorder:CmpDocBorder",
+		},
+	},
+}
+
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = "rafamadriz/friendly-snippets",
+			opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+			config = function(_, opts)
+				require("luasnip").config.set_config(opts)
+				require("luasnip.loaders.from_vscode").lazy_load({
+					exclude = vim.g.vscode_snippets_exclude or {},
+				})
+				require("luasnip.loaders.from_vscode").lazy_load({
+					paths = vim.g.vscode_snippets_path or "",
+				})
+			end,
+		},
+		{
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+		},
+	},
+	opts = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+
+		return {
+			completion = options.completion,
+
+			formatting = {
+				fields = { "abbr", "kind", "menu" },
+				format = require("lspkind").cmp_format({
+					mode = "symbol_text",
+					maxwidth = 50,
+					ellipsis_char = "...",
+					show_labelDetails = true,
+				}),
+			},
+
+			mapping = {
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
+
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+
+			sources = options.sources,
+
+			window = options.window,
+		}
+	end,
+}
